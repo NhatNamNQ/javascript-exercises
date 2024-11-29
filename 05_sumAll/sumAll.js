e89b27952f5cfbc998c61d75666c8f5e59cc8a4e
NhatNamNQ <138838322+NhatNamNQ@users.noreply.github.com>
2024-11-29 09:33:45 +0000

    Commited 05-sumAl exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(beginNumber, endNumber) {
+    if (beginNumber < 0 || endNumber < 0) {
+        return "ERROR";
+    }
 
+    if (!Number.isInteger(beginNumber) || !Number.isInteger(endNumber)) {
+        return "ERROR";
+    }
+    let sum = 0;
+
+    if (beginNumber > endNumber) {
+        let tmp = beginNumber;
+        beginNumber = endNumber;
+        endNumber = tmp;
+    }
+
+    for (let i = beginNumber; i <= endNumber; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
