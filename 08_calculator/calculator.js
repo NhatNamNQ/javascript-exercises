e0d0a2dd8e092d19273c8bd696c0ff67208b1751
NhatNamNQ <138838322+NhatNamNQ@users.noreply.github.com>
2024-12-04 07:47:55 +0000

    Completed 08-calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr = []) {
+  return arr.reduce((total, item) => total + item, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr = []) {
+  return arr.reduce((product, item) => product * item, 1);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  let product = 1;
+  for (let i = 0; i < b; i++) {
+    product *= a;
+  }
+  return product;
 };
 
-const factorial = function() {
-	
+const factorial = function (number) {
+  let factorial = 1;
+  for (let i = number; i > 1; i--) {
+    factorial *= i;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
