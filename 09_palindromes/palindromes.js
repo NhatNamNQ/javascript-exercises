78d7a8759abe6d75140e113de8e8a7bdb17514e9
NhatNamNQ <138838322+NhatNamNQ@users.noreply.github.com>
2024-12-04 11:01:12 +0000

    Completed 09-palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (words = "") {
+    let splitWord = words.split("");
+    splitWord = splitWord.map((character) => character.toLowerCase());
+    let filterCharacter = splitWord.filter((char) => char >= "a" && char <= "z" || char >="0" && char <= "9");
+    words = filterCharacter.join("");
+    let reverseWords = filterCharacter.reverse().join("");
+    return words === reverseWords;
 };
 
 // Do not edit below this line
