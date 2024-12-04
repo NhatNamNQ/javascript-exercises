1972114b5924724d08e7098e368c4008998aa655
NhatNamNQ <138838322+NhatNamNQ@users.noreply.github.com>
2024-12-04 14:00:29 +0000

    Completed 12-findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,14 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function (people = []) {
+    let yearsOfPeople = people.map((person) => {
+      if (!person.yearOfDeath) {
+        person.years = (new Date()).getFullYear() - person.yearOfBirth;
+        return person;
+      }
+      person.years = person.yearOfDeath - person.yearOfBirth;
+      return person;
+    }).sort((a, b) => b.years - a.years);
+    return yearsOfPeople[0];
+  };
 
 // Do not edit below this line
 module.exports = findTheOldest;
